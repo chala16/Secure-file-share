2322cbb34f618d97bf71eb37979cfa3b2b643d2c
chala16 <it22338952@my.sliit.lk>
2025-08-07 22:13:08 +0530

    JWT Token Generation
    Add function to generate JWT tokens using user ID and secret from environment variables

--- utils/SecretToken.js
+++ utils/SecretToken.js
@@ -1,2 +1,8 @@
-require(dotenv).config();
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
+module.exports.createSecretToken = (userId) => {    
+    return jwt.sign({ userId }, process.env.TOKEN_SECRET, {
+        expiresIn: '1h'
+    });
+}
